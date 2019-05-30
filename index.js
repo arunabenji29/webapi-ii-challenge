6a5ee38c9cfeab76e7d8cbf983f46e0ea26c2925
Aruna Benjamin <arunabenji29@gmail.com>
2019-05-30 14:21:19 -0600

    changes made to deploy

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config()
+
 const server = require('./api/server.js')
 
-server.listen(3333, ()=> {
-    console.log('API running on http://localhost:3333')
+const port = process.env.PORT || 3333
+
+server.listen(port, ()=> {
+    console.log(`API running on http://localhost:${port}`)
 });
